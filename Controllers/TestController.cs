0afee5c9be26682a60c846f352e89e367d2ca9c4
Trung Nguyen <trungnguyenvan1988@gmail.com>
2025-06-20 10:48:30 +0700

    Initial commit - ASP.NET Core Web API

---
+++ Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApi.DTOs;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            return Ok("Hello Trung! API is working!");
+        }
+
+        [HttpGet("products")]
+        public IActionResult GetFakeProducts()
+        {
+            var products = new List<ProductDto>
+            {
+                new ProductDto { Id = 1, Name = "Apple", Price = 10.5 },
+                new ProductDto { Id = 2, Name = "Mango", Price = 20 }
+            };
+            return Ok(products);
+        }
+    }
+}
---
+++ DTOs/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductApi.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
